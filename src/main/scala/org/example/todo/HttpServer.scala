11a931918341debf96f0a53578d980ff7b89660a
Vlad Povarna <povarna@adobe.com>
2022-11-24 18:07:18 +0200

    Adding prometheus endpoint

--- src/main/scala/org/example/todo/HttpServer.scala
+++ src/main/scala/org/example/todo/HttpServer.scala
@@ -2,6 +2,7 @@ package org.example.todo
 
 import scala.concurrent.ExecutionContext
 
+import cats.data.Kleisli
 import cats.effect._
 import cats.effect.kernel.Resource
 import doobie.ExecutionContexts
@@ -11,7 +12,10 @@ import org.example.todo.config.{AppConfig, Config}
 import org.example.todo.db.Database
 import org.example.todo.repository.Dao
 import org.example.todo.service.Services
+import org.http4s.metrics.MetricsOps
+import org.http4s.metrics.prometheus.{Prometheus, PrometheusExportService}
 import org.http4s.server.blaze.BlazeServerBuilder
+import org.http4s.{HttpRoutes, Request, Response}
 
 object HttpServer {
 
@@ -22,7 +26,12 @@ object HttpServer {
     config <- AppConfig.load(configFile)
     ec <- ExecutionContexts.fixedThreadPool[IO](config.database.threadPoolSize)
     xa <- Database.transactor(config.database, ec)
-  } yield Resources(config, xa)
+    metricsService <- PrometheusExportService.build[IO]
+    metrics <- Prometheus.metricsOps[IO](
+      metricsService.collectorRegistry,
+      config.metrics.prefixName
+    )
+  } yield Resources(config, xa, MetricsComponents(metrics, metricsService))
 
   private def create(resources: Resources): IO[ExitCode] =
     for {
@@ -31,20 +40,36 @@ object HttpServer {
           port = resources.config.server.port,
           host = resources.config.server.host
         )
-        .withHttpApp(httpApp = getHttpRoutes(resources.transactor))
+        .withHttpApp(httpApp =
+          getHttpRoutes(resources.transactor, resources.metricsComponents)
+        )
         .serve
         .compile
         .lastOrError
     } yield exitCode
 
   // Wiring
-  private def getHttpRoutes(xa: Transactor[IO]) = {
+  private def getHttpRoutes(
+      xa: Transactor[IO],
+      metricsResources: MetricsComponents
+  ): Kleisli[IO, Request[IO], Response[IO]] = {
     val dao: Dao = new Dao(xa)
     val services: Services = new Services(dao)
-    val api: Apis = new Apis(services)
-    api.httpRoutes
+    val api: Apis = new Apis(services, metricsResources)
+    val metricsRoutes: HttpRoutes[IO] = metricsResources.metricsService.routes
+    val todoHttpRoutes: Kleisli[IO, Request[IO], Response[IO]] = api.httpRoutes
+    todoHttpRoutes
   }
 
-  final case class Resources(config: Config, transactor: Transactor[IO])
+  final case class Resources(
+      config: Config,
+      transactor: Transactor[IO],
+      metricsComponents: MetricsComponents
+  )
+
+  final case class MetricsComponents(
+      metricsOps: MetricsOps[IO],
+      metricsService: PrometheusExportService[IO]
+  )
 
 }
--- src/main/scala/org/example/todo/api/Apis.scala
+++ src/main/scala/org/example/todo/api/Apis.scala
@@ -2,18 +2,24 @@ package org.example.todo.api
 
 import cats.data.Kleisli
 import cats.effect.IO
+import org.example.todo.HttpServer.MetricsComponents
 import org.example.todo.service.Services
 import org.http4s.implicits._
+import org.http4s.metrics.prometheus.PrometheusExportService
 import org.http4s.server.Router
-import org.http4s.{Request, Response}
+import org.http4s.server.middleware.Metrics
+import org.http4s.{HttpRoutes, Request, Response}
 
-class Apis(service: Services) {
-  val genericApi = new GenericApi(service.genericService)
-  val todoApi = new TodoApi(service.todoService)
+class Apis(service: Services, metricsComponents: MetricsComponents) {
+  private val genericApi: GenericApi = new GenericApi(service.genericService)
+  private val todoApi: TodoApi = new TodoApi(service.todoService)
+  private val metricsService: PrometheusExportService[IO] =
+    metricsComponents.metricsService
 
   val httpRoutes: Kleisli[IO, Request[IO], Response[IO]] = Router(
+    "/" -> metricsService.routes,
     "/" -> genericApi.routes,
-    "todos" -> todoApi.routes
+    "todos" -> Metrics[IO](metricsComponents.metricsOps)(todoApi.routes)
   ).orNotFound
 
 }
